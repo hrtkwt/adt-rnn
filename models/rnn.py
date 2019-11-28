e738e77442ebcab08e5877cb2832a7579b138e28
hrtkwt(gsim02) <cs181004@meiji.ac.jp>
2019-11-28 12:56:20 +0900

    pesudo training

--- models/rnn.py
+++ models/rnn.py
@@ -3,30 +3,24 @@ import tensorflow as tf
 
 def train(X_train, X_valid, Y_train, Y_valid, params):
 
-    BATCH_SIZE = 10
-    EPOCHS = 20
-
     # model
     model = tf.keras.models.Sequential([
-        tf.keras.layers.SimpleRNN(200, activation='sigmoid'),
-        tf.keras.layers.Dense(3, activation='sigmoid')
+        tf.keras.layers.SimpleRNN(params["n_hidden"], activation=params["activation"]),
+        tf.keras.layers.Dense(params["n_out"], activation='sigmoid')
     ])
 
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(optimizer=params["optimizer"],
+                  loss=params["loss"],
+                  metrics=[params["metrics"]])
 
+    # set callback
     tensorboard_callback = ""
     cp_callback = ""
 
     # train
     model.fit(X_train,
               Y_train,
-              batch_size=BATCH_SIZE,
-              epochs=EPOCHS,
-              callbacks=[tensorboard_callback, cp_callback],
-              validation_split=0.2)
-
-
-if __name__ == '__main__':
-    print("model")
+              batch_size=params["batch_size"],
+              epochs=params["epochs"],
+              #callbacks=[tensorboard_callback, cp_callback],
+              validation_data=(X_valid, Y_valid))
--- train.py
+++ train.py
@@ -1,20 +1,29 @@
+import sys
+import argparse
+import json
+
 from sklearn.model_selection import KFold
-from utils.data import load_datasets, load_target
+
+from utils.data import load_train_data, load_train_target
 from models.rnn import train
 
 
 # load parameter
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', default='./configs/train.json')
+options = parser.parse_args()
+config = json.load(open(options.config))
 
-X_train_all, X_test = load_datasets(feats="")
-Y_train_all = load_target(target_name="")
+# load dataset
+X_train_all = load_train_data(date=config["date"])
+Y_train_all = load_train_target(date=config["date"])
 
-rnn_params = ""
 
 kf = KFold(n_splits=3, random_state=0)
 
 for train_index, valid_index in kf.split(X_train_all):
 
-    X_train, X_valid = X_train_all[train_index, :], X_train_all[valid_index, :]
+    X_train, X_valid = X_train_all[train_index], X_train_all[valid_index]
     Y_train, Y_valid = Y_train_all[train_index], Y_train_all[valid_index]
 
-    train(X_train, X_valid, Y_train, Y_valid, rnn_params)
+    train(X_train, X_valid, Y_train, Y_valid, config["params"])
