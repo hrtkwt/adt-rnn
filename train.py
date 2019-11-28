dc9bd0d5f4da368ae2fafc1a02ecaded5d11e2eb
Hiroto Kawata <cs181004@meiji.ac.jp>
2019-11-28 11:01:35 +0900

    rename run.py train.py

--- train.py
+++ train.py
@@ -1,15 +1,22 @@
 from sklearn.model_selection import KFold
-from utils import load_datasets, load_target
+from utils.data import load_datasets, load_target
+from models.rnn import train
+
 
 # load parameter
 
 X_train_all, X_test = load_datasets(feats="")
-y_train_all = load_target(target_name="")
+Y_train_all = load_target(target_name="")
+
+rnn_params = ""
 
 y_preds = []
 models = []
 
 kf = KFold(n_splits=3, random_state=0)
 for train_index, valid_index in kf.split(X_train_all):
+
     X_train, X_valid = X_train_all[train_index, :], X_train_all[valid_index, :]
-    y_train, y_valid = y_train_all[train_index], y_train_all[valid_index]
+    Y_train, Y_valid = Y_train_all[train_index], Y_train_all[valid_index]
+
+    train(X_train, X_valid, Y_train, Y_valid, rnn_params)
