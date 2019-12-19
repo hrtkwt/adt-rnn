0bfcc8c942d9429aa58c093fde5a7bc6e4c6e011
hrtkwt(gsim02) <cs181004@meiji.ac.jp>
2019-12-19 11:07:55 +0900

    run.sh 引数を考慮

--- run.sh
+++ run.sh
@@ -1,23 +1,30 @@
-
-
-
 feature() {
     python feature.py m
     python feature.py acd
     python feature.py md
+    python feature.py m_md
+    python feature.py m_acd
 }
 
 train() {
-    python train.py K_m
-    python train.py K_acd
-    python train.py K_md
+    python train.py L_m base_1025
+    python train.py L_acd base_1025
+    python train.py L_md base_1025
+    python train.py L_m_md base_2050
+    python train.py L_m_acd base_2050
 }
 
 eval() {
-    python eval_all.py J_m 7
-    python eval_all.py J_acd 8
-    python eval_all.py J_md 7
+    python eval_all.py K_m 6
+    python eval_all.py K_acd 5
+    python eval_all.py K_md 5
 }
 
 
-train
+if [ "$1" = "feature" ]; then
+    feature
+elif [ "$1" = "train" ]; then
+    train
+elif [ "$1" = "eval" ]; then
+    eval
+fi
